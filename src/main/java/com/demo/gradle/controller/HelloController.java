f12dd5a456ccee10049d79f4bc4574ea763df13f
wanxiaolong <wanxiaolong100@163.com>
2024-12-18 12:15:08 +0800

    [jira-006]: integrate with springboot

--- src/main/java/com/demo/gradle/controller/HelloController.java
+++ src/main/java/com/demo/gradle/controller/HelloController.java
@@ -1,6 +1,14 @@
 package com.demo.gradle.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
+  @GetMapping("/hello")
+  public String hello(@RequestParam("name") String name) {
+    return "Hello " + name;
+  }
 }
